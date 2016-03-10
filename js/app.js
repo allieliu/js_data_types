4a3efd8edbdb72635754fa4c5f32aa630740bcc8
Allie Liu <allieliu@outlook.com>
2016-03-09 17:23:46 -0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,22 @@
+//var flowers = ["rose", "tulip", "orchid", "daisy"];
+
+//var index = 0;
+//while(index < flowers.length) {
+  //console.log(flowers[index]);
+  //index++;
+//}
+
+
+//var beatles = ["George", "Paul", "Ringo", "John"];
+//var index = 0;
+//while(index < beatles.length) {
+//  console.log("Hello, chap. My name iseth " + beatles[index]);
+  //index++;
+//}
+
+var beatles = ["John", "Paul", "George", "Ringo"];
+for(var i = beatles.length - 1; i > -1; i--) {
+    console.log("Hello, my name is " + beatles[i])
+}
+
+  //semi-colons not commas in JS!!
